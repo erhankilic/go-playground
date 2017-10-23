506e464ca7fb83a63272ddd7553c8e75ff377f23
Erhan Kılıç <erhan_kilic@outlook.com>
2017-10-23 18:59:44 +0300

    Flow Control finished.

---
+++ go-tour/2-flowcontrol/11.go
@@ -0,0 +1,27 @@
+/*
+
+Switch with no condition
+
+Switch without a condition is the same as switch true.
+
+This construct can be a clean way to write long if-then-else chains.
+*/
+
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	t := time.Now()
+	switch {
+	case t.Hour() < 12:
+		fmt.Println("Good Morning!")
+	case t.Hour() < 17:
+		fmt.Println("Good Afternoon.")
+	default:
+		fmt.Println("Good evening.")
+	}
+}
